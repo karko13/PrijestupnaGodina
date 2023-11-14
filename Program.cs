05ba38e4d38348d3d8854c582c43873eccadc274
karko13 <145532713+karko13@users.noreply.github.com>
2023-11-14 19:29:53 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IspitVjezba2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Startgod = 1900, endGod = 2100;
+            for (int i = Startgod; i <= endGod; i++)
+            {
+                if ((i % 400 == 0) || (i % 4 == 0 && i % 100 != 0))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
